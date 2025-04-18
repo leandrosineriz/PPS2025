a4ca90d912540e9ed7723ee91942080a3f767415
leandrosineriz <leandrosineriz@gmail.com>
2025-04-17 22:43:49 -0300

    se agrega icon y splash screen

--- Proyecto1/login/capacitor.config.ts
+++ Proyecto1/login/capacitor.config.ts
@@ -2,8 +2,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'pps.login.app',
-  appName: 'login',
-  webDir: 'www'
+  appName: 'PPS Login',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Duración en milisegundos
+      //launchAutoHide: true, // Se ocultará automáticamente después del tiempo especificado
+      androidSplashResourceName: 'Default', // Nombre del recurso de imagen para Android
+      //iosSplashResourceName: 'Default', // Nombre del recurso de imagen para iOS
+      showSpinner: false, // Muestra un spinner de carga
+      //splashFullScreen: true,
+      //splashImmersive: true,
+    }
+  }
 };
 
 export default config;
--- Proyecto1/login/src/app/login/login.page.ts
+++ Proyecto1/login/src/app/login/login.page.ts
@@ -13,22 +13,15 @@ import { SupabaseService } from '../services/supabase/supabase.service';
 export class LoginPage implements OnInit {
   password: string = '';
   user: string = '';
-  isLogin: boolean = false
-  type: boolean = true;
   showContent: boolean = false; // Inicialmente oculto error
-
-  
 
   constructor(private authService: AuthService, private router:Router, private supabase: SupabaseService) {}
 
 
   ngOnInit() {
-    console.log("Ingreso a Login");
+    //console.log("Ingreso a Login");
   }
 
-  changeType() {
-    this.type = !this.type; // Cambia el tipo de input entre password y text
-  }
 
   onSubmit(form: NgForm) {
     this.login(form.value.user, form.value.password);
@@ -39,56 +32,35 @@ export class LoginPage implements OnInit {
   // Funcion de login con localstorage
   login(user: string, password: string) {
     console.log("Mail:" + user + " \nPassword: " + password);
-    this.isLogin = true;
+  
     this.supabase.login(user, password).then(async data => {  
-      //console.log(data);
       if (data.session.access_token) {
         await this.authService.setItem('uid', data); // Guardar el usuario en el almacenamiento local
         this.showContent = false; // Muestra el contenido después de iniciar sesión
         this.router.navigateByUrl('/tabs');
-        this.isLogin = false;
       } else {
-        this.showContent = true; // Oculta el contenido si las credenciales son incorrectas
-        this.isLogin = false;
+        this.showContent = true; // Oculta el contenido si las credenciales son incorrectas   
         console.log("Password o usuario incorrecto.");
       }
     })
     .catch(err => { 
+      this.showContent = true;
       console.log(err);
     });
-    /*this.authService.login(form.value.user, form.value.password).then(data => {
-      if (data) {
-        this.showContent = false; // Muestra el contenido después de iniciar sesión
-        this.router.navigateByUrl('/tabs');
-        this.isLogin = false;
-        form.reset();
-      } else {
-        this.showContent = true; // Oculta el contenido si las credenciales son incorrectas
-        this.isLogin = false;
-        console.log("Password o usuario incorrecto.");
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      this.isLogin = false;
-    });*/
   }
 
   loginVisitante() {
-    //this.login("visitor@visitor.com", "visitor");
     this.user = "visitor@visitor.com";
     this.password = "visitor";
 
   }
 
   loginUsuario() {
-    //this.login("user@user.com", "user");
     this.user = "user@user.com";
     this.password = "user";
   }
 
   loginAdmin () {
-    //this.login("admin@admin.com", "admin");
     this.user = "admin@admin.com";
     this.password = "admin";
   }
